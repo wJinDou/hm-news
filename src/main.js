75a34cb601d4778f5e64800ac877630cf9b9a88c
wJinDou <478956917@qq.com>
2020-08-26 17:51:21 +0800

    配置初始化文件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入通用样式
+import './styles/base.less'
+import './styles/iconfont.css'
+
+// 导入所有组件的方式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
+// 按需加载
+// import { Button } from 'vant'
+// Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 导入组件
+import Login from '../views/Login.vue'
+import Resgiter from '../views/Resgiter.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/login', component: Login },
+  { path: '/resgiter', component: Resgiter }
 ]
 
 const router = new VueRouter({
